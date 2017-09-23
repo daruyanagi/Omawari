dab90a63a99e478016f6f46d8e8dc52e03ce664c
daruyanagi <daruyanagi@gmail.com>
2017-09-23 14:13:53 +0900

    Chocolatey パッケージのためのスクリプトを追加

---
+++ Omawari/Setup/Build.ps1
@@ -0,0 +1,32 @@
+cd "$PSScriptRoot" 
+
+# tools フォルダーを空にする
+remove-item ".\tools" -recurse -force
+
+# Release ビルドを tools フォルダーへコピー
+$source = "..\bin\Release" 
+robocopy $source .\tools /MIR
+
+# 実行ファイルのシミングを防止
+$files = get-childitem ".\tools" -include *.exe -recurse
+foreach ($file in $files) {
+  #generate an ignore file
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+# インストールスクリプトなどをコピー
+copy .\chocolateyinstall.ps1 .\tools\chocolateyinstall.ps1
+copy .\LICENSE.txt .\tools\LICENSE.txt
+copy .\VERIFICATION.txt .\tools\VERIFICATION.txt
+
+# .nuspec のバージョンを書き換え
+$version = (Get-ItemProperty ..\bin\Release\Omawari.exe).VersionInfo.FileVersion
+$(Get-Content .\omawari.nuspec.sample) -replace "<version>.+?</version>", "<version>$version</version>" > .\omawari.nuspec
+
+# .nupkg を再生成
+del *.nupkg
+choco pack
+
+# .nupkg のテスト
+choco uninstall "Omawari"
+choco install "Omawari" -fdv -y  -s ".\" 
---
+++ Omawari/Setup/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = "Omawari"
+$target = Join-Path $toolsDir "$($packageName).exe"
+$shortcut = [Environment]::GetFolderPath("Desktop")
+$shortcut = Join-Path $shortcut "Omawari.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath $shortcut -TargetPath $target
